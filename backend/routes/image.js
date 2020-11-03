56903c6eca2023ed76bc5275b698ad8dc286bb0e
kingd0mz <dcfargas@up.edu.ph>
2020-11-03 16:55:49 +0800

    updated image model and routes

--- backend/routes/image.js
+++ backend/routes/image.js
@@ -1,53 +1,59 @@
 const router = require('express').Router();
-let imgModel = require('../models/image.model');
+let Image = require('../models/image.model');
 
-var imgPath = '../conv3.png';
-
-var fs = require('fs'); 
-var path = require('path'); 
-var multer = require('multer'); 
-
-var storage = multer.diskStorage({ 
-	destination: (req, file, cb) => { 
-		cb(null, 'uploads') 
-	}, 
-	filename: (req, file, cb) => { 
-		cb(null, file.fieldname + '-' + Date.now()) 
-	} 
-}); 
-
-var upload = multer({ storage: storage }); 
+const mongoose = require('mongoose');
+const fs = require('fs');
+const path = require('path');
 
 // Retriving the image 
 router.route('/').get((req, res) => { 
-    imgModel.find({}, (err, items) => { 
+    Image.find({}, (err, items) => { 
         if (err) { 
             console.log(err); 
         } 
-        else { 
+        else {
             res.render('app', { items: items }); 
-        } 
-    }); 
-}); 
+        }
+    });
+});
 
-// Uploading the image 
-router.route('/').post(upload.single('image'), (req, res, next) => { 
-  
-    var obj = { 
-        name: req.body.name, 
-        desc: req.body.desc, 
-        img: { 
-            data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)), 
-            contentType: 'image/png'
-        } 
-    } 
-    imgModel.create(obj, (err, item) => { 
-        if (err) { 
-            console.log(err); 
-        } 
-        else { 
-            // item.save(); 
-            res.redirect('/'); 
-        } 
-    }); 
-}); 
+//adds Image upon Submit
+// dapat may kukunin to na path pagka-upload, tas yun ilalagay sa imgPath na variable
+router.route('/upload').post((req, res) => {
+    const imgPath = __dirname.replace('routes','conv3.png');
+    const userID = "ilalagay dito si objectID ni user pang link";
+    const reportID = "ilalagay dito si objectID ni report pang link";
+    const imageBuffer = fs.readFileSync(imgPath);
+    // To convert to base64, imageBuffer.toString('base64')
+    const imageType = 'image/'+path.extname(imgPath).replace('.','').toLowerCase();
+
+    const newItem = new Image({
+        userID,
+        reportID,
+        image: {
+                data: imageBuffer,
+                contentType: imageType
+            }
+    });
+
+    newItem.save()
+            .then(() => res.json('Image uploaded!'))
+            .catch(err => res.status(400).json('Error: ' + err))
+
+    // const checkUser = {_id: mongoose.Types.ObjectId(req.body.userID)};
+
+    // var result = User.findOne(checkUser, (err, result) => {
+    //     if (result == null){
+    //         res.status(400).send("User is not found in database");
+    //     } else {
+
+    //     newItem.save()
+    //         .then(() => res.json('Report added!'))
+    //         .catch(err => res.status(400).json('Error: ' + err))
+    //     res.status(200).send(result);
+    //     }
+    // });
+}
+);
+
+module.exports = router;
