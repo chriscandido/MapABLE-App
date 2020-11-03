10a0ec99f2cc20a13af188aeb83b1918b29be17e
Gilson Narciso <gmnarciso@up.edu.ph>
2020-11-03 17:50:47 +0800

    updated server uri

--- backend/server.js
+++ backend/server.js
@@ -13,9 +13,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 
 //MongoDB connection string
-const uri = "mongodb+srv://mapable:mapable@mapable-cluster.fjjjm.mongodb.net/mapable-db?retryWrites=true&w=majority";
-
-// "mongodb+srv://mapable:mapable@mapable-cluster.fjjjm.mongodb.net/mapable-db?retryWrites=true&w=majority"
+const uri = "mongodb+srv://mapable:mapable@mapable-cluster.fjjjm.mongodb.net/mapable-db?retryWrites=true&w=majority"
 mongoose.connect(process.env.MONGODB_URI || uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }).catch(error => console.log(error));
 
 const connection = mongoose.connection;
@@ -28,11 +26,11 @@ app.use(bodyParser.json())
 
 const userRouter = require('./routes/user');
 const reportRouter = require('./routes/report');
-// const imageRouter = require('./routes/image');
+const imageRouter = require('./routes/image');
 
 app.use('/users', userRouter);
 app.use('/reports', reportRouter);
-// app.use('/images', imageRouter);
+app.use('/images', imageRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
