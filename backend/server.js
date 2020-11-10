dff3d41aad1e2ea1e373010b47468e1ad7de29c1
kingd0mz <dcfargas@up.edu.ph>
2020-11-10 13:10:45 +0800

    increased JSON limit, function for uncompressed image

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 
 //MongoDB connection string
 const uri = "mongodb+srv://mapable:mapable@mapable-cluster.fjjjm.mongodb.net/mapable-db?retryWrites=true&w=majority"
@@ -21,8 +21,8 @@ connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
+app.use(bodyParser.json({limit: '50mb'}))
 // Set EJS as templating engine
 app.set("view engine", "ejs");
 
