0f39d4386f1bfe662ba511bc74462a83106fafcc
cgcandido <chriscandido93@gmail.com>
2021-02-23 13:14:50 +0800

    Update UI

--- app/src/main/java/up/envisage/mapable/ui/home/AboutManilaBayActivity.java
+++ app/src/main/java/up/envisage/mapable/ui/home/AboutManilaBayActivity.java
@@ -1,9 +1,12 @@
 package up.envisage.mapable.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import up.envisage.mapable.MainActivity;
 import up.envisage.mapable.R;
 
 public class AboutManilaBayActivity extends AppCompatActivity {
@@ -12,5 +15,12 @@ public class AboutManilaBayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aboutmanilabay);
+
+        TextView textView_informationManilaBay_back = findViewById(R.id.textView_informationManilaBay_back);
+
+        textView_informationManilaBay_back.setOnClickListener(v -> {
+            Intent back = new Intent(AboutManilaBayActivity.this, MainActivity.class);
+            startActivity(back);
+        });
     }
 }
