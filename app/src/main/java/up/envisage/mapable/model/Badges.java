5c4bdedd822d72c02b7fb66b7d847b0bc9df9b9d
Gilson Narciso <gmnarciso@up.edu.ph>
2021-12-01 17:13:19 +0800

    Separated badges and fixed report view

---
+++ app/src/main/java/up/envisage/mapable/model/Badges.java
@@ -0,0 +1,50 @@
+package up.envisage.mapable.model;
+
+public class Badges {
+
+    public String algalBloom;
+    public String fishKill;
+    public String waterPollution;
+    public String ongoingReclamation;
+    public String waterHyacinth;
+    public String solidWaste;
+    public String total;
+    public String verified;
+    public String oneOfAKind;
+
+    public String getAlgalBloom() {
+        return algalBloom;
+    }
+
+    public String getFishKill() {
+        return fishKill;
+    }
+
+    public String getWaterPollution() {
+        return waterPollution;
+    }
+
+    public String getOngoingReclamation() {
+        return ongoingReclamation;
+    }
+
+    public String getWaterHyacinth() {
+        return waterHyacinth;
+    }
+
+    public String getSolidWaste() {
+        return solidWaste;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public String getVerified() {
+        return verified;
+    }
+
+    public String getOneOfAKind() {
+        return oneOfAKind;
+    }
+}
---
+++ app/src/main/java/up/envisage/mapable/model/Image.java
@@ -0,0 +1,11 @@
+package up.envisage.mapable.model;
+
+public class Image {
+
+    public Object imageString;
+
+    public Object getImage() {
+        return imageString;
+    }
+
+}
